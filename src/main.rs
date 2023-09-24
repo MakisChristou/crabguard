e84a718efab7861e0873f6c40ad785ed3dc8607a
Makis Christou <michalis.christou@protonmail.com>
2023-09-24 12:51:40 +0300

    Refactor encryption/decryption to their own functions

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,45 @@ impl NonceSequence for CounterNonceSequence {
 
         let bytes = self.0.to_be_bytes();
         nonce_bytes[8..].copy_from_slice(&bytes);
-        println!("nonce_bytes = {}", hex::encode(&nonce_bytes));
 
         self.0 += 1; // advance the counter
         Nonce::try_assume_unique_for_key(&nonce_bytes)
     }
+}
+
+fn encrypt(
+    data: Vec<u8>,
+    key_bytes: Vec<u8>,
+    nonce_sequence: CounterNonceSequence,
+) -> Result<Vec<u8>, Unspecified> {
+    // Create a new AEAD key without a designated role or nonce sequence
+    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
+
+    // Create a new AEAD key for encrypting and signing ("sealing"), bound to a nonce sequence
+    // The SealingKey can be used multiple times, each time a new nonce will be used
+    let mut sealing_key = SealingKey::new(unbound_key, nonce_sequence);
+
+    // Create a mutable copy of the data that will be encrypted in place
+    let mut in_out = data.clone();
+
+    // Encrypt the data with AEAD using the AES_256_GCM algorithm
+    let tag = sealing_key.seal_in_place_separate_tag(Aad::empty(), &mut in_out)?;
+
+    // Decrypt the data by passing in the associated data and the cypher text with the authentication tag appended
+    let cypher_text_with_tag = [&in_out, tag.as_ref()].concat();
+
+    Ok(cypher_text_with_tag)
+}
+
+fn decrypt(
+    mut cypher_text_with_tag: Vec<u8>,
+    key_bytes: Vec<u8>,
+    nonce_sequence: CounterNonceSequence,
+) -> Result<Vec<u8>, Unspecified> {
+    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
+    let mut opening_key = OpeningKey::new(unbound_key, nonce_sequence);
+    let decrypted_data = opening_key.open_in_place(Aad::empty(), &mut cypher_text_with_tag)?;
+    Ok(decrypted_data.to_vec())
 }
 
 fn main() -> Result<(), Unspecified> {
@@ -48,50 +82,13 @@ fn main() -> Result<(), Unspecified> {
         }
     };
 
-    println!("Key Length is {} bytes", key_bytes.len());
-    println!("key_bytes = {}", hex::encode(&key_bytes)); // don't print this in production code
-
-    // Create a new AEAD key without a designated role or nonce sequence
-    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
-
-    // Create a new NonceSequence type which generates nonces
-    let nonce_sequence = CounterNonceSequence(1);
-
-    // Create a new AEAD key for encrypting and signing ("sealing"), bound to a nonce sequence
-    // The SealingKey can be used multiple times, each time a new nonce will be used
-    let mut sealing_key = SealingKey::new(unbound_key, nonce_sequence);
-
-    // This data will be authenticated but not encrypted
-    //let associated_data = Aad::empty(); // is optional so can be empty
-    let associated_data = Aad::from(b"additional public data");
-
-    // Data to be encrypted
-    // let data = b"hello world";
     let data = fs::read(args.target).unwrap();
-    // println!("data = {}", String::from_utf8(data.to_vec()).unwrap());
 
-    // Create a mutable copy of the data that will be encrypted in place
-    let mut in_out = data.clone();
-
-    // Encrypt the data with AEAD using the AES_256_GCM algorithm
-    let tag = sealing_key.seal_in_place_separate_tag(associated_data, &mut in_out)?;
-
-    // Recreate the previously moved variables
-    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
     let nonce_sequence = CounterNonceSequence(1);
-    //let associated_data = Aad::empty(); // supplying the wrong data causes the decryption to fail
-    let associated_data = Aad::from(b"additional public data");
+    let cypher_text_with_tag = encrypt(data.clone(), key_bytes.clone(), nonce_sequence)?;
 
-    // Create a new AEAD key for decrypting and verifying the authentication tag
-    let mut opening_key = OpeningKey::new(unbound_key, nonce_sequence);
-
-    // Decrypt the data by passing in the associated data and the cypher text with the authentication tag appended
-    let mut cypher_text_with_tag = [&in_out, tag.as_ref()].concat();
-    let decrypted_data = opening_key.open_in_place(associated_data, &mut cypher_text_with_tag)?;
-    // println!(
-    //     "decrypted_data = {}",
-    //     String::from_utf8(decrypted_data.to_vec()).unwrap()
-    // );
+    let nonce_sequence = CounterNonceSequence(1);
+    let decrypted_data = decrypt(cypher_text_with_tag, key_bytes, nonce_sequence)?;
 
     assert_eq!(data, decrypted_data);
     Ok(())
