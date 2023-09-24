ce6857f8f241e56c917a6157fed214d985f5b410
Makis Christou <michalis.christou@protonmail.com>
2023-09-24 12:57:35 +0300

    Move encrypt/decrypt to utils module

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,15 @@
 use std::fs;
-
 use clap::Parser;
-use ring::aead::Aad;
-use ring::aead::BoundKey;
-use ring::aead::Nonce;
-use ring::aead::NonceSequence;
-use ring::aead::OpeningKey;
-use ring::aead::SealingKey;
-use ring::aead::UnboundKey;
-use ring::aead::AES_256_GCM;
-use ring::aead::NONCE_LEN;
 use ring::error::Unspecified;
-
-use crate::args::Args;
 use dotenv::dotenv;
 use std::env;
+
+use crate::args::Args;
+
 
 mod args;
 mod utils;
 
-struct CounterNonceSequence(u32);
-
-impl NonceSequence for CounterNonceSequence {
-    // called once for each seal operation
-    fn advance(&mut self) -> Result<Nonce, Unspecified> {
-        let mut nonce_bytes = vec![0; NONCE_LEN];
-
-        let bytes = self.0.to_be_bytes();
-        nonce_bytes[8..].copy_from_slice(&bytes);
-
-        self.0 += 1; // advance the counter
-        Nonce::try_assume_unique_for_key(&nonce_bytes)
-    }
-}
-
-fn encrypt(
-    data: Vec<u8>,
-    key_bytes: Vec<u8>,
-    nonce_sequence: CounterNonceSequence,
-) -> Result<Vec<u8>, Unspecified> {
-    // Create a new AEAD key without a designated role or nonce sequence
-    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
-
-    // Create a new AEAD key for encrypting and signing ("sealing"), bound to a nonce sequence
-    // The SealingKey can be used multiple times, each time a new nonce will be used
-    let mut sealing_key = SealingKey::new(unbound_key, nonce_sequence);
-
-    // Create a mutable copy of the data that will be encrypted in place
-    let mut in_out = data.clone();
-
-    // Encrypt the data with AEAD using the AES_256_GCM algorithm
-    let tag = sealing_key.seal_in_place_separate_tag(Aad::empty(), &mut in_out)?;
-
-    // Decrypt the data by passing in the associated data and the cypher text with the authentication tag appended
-    let cypher_text_with_tag = [&in_out, tag.as_ref()].concat();
-
-    Ok(cypher_text_with_tag)
-}
-
-fn decrypt(
-    mut cypher_text_with_tag: Vec<u8>,
-    key_bytes: Vec<u8>,
-    nonce_sequence: CounterNonceSequence,
-) -> Result<Vec<u8>, Unspecified> {
-    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
-    let mut opening_key = OpeningKey::new(unbound_key, nonce_sequence);
-    let decrypted_data = opening_key.open_in_place(Aad::empty(), &mut cypher_text_with_tag)?;
-    Ok(decrypted_data.to_vec())
-}
 
 fn main() -> Result<(), Unspecified> {
     let args = Args::parse();
@@ -84,11 +26,11 @@ fn main() -> Result<(), Unspecified> {
 
     let data = fs::read(args.target).unwrap();
 
-    let nonce_sequence = CounterNonceSequence(1);
-    let cypher_text_with_tag = encrypt(data.clone(), key_bytes.clone(), nonce_sequence)?;
+    let nonce_sequence = utils::CounterNonceSequence(1);
+    let cypher_text_with_tag = utils::encrypt(data.clone(), key_bytes.clone(), nonce_sequence)?;
 
-    let nonce_sequence = CounterNonceSequence(1);
-    let decrypted_data = decrypt(cypher_text_with_tag, key_bytes, nonce_sequence)?;
+    let nonce_sequence = utils::CounterNonceSequence(1);
+    let decrypted_data = utils::decrypt(cypher_text_with_tag, key_bytes, nonce_sequence)?;
 
     assert_eq!(data, decrypted_data);
     Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,32 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+use ring::aead::NONCE_LEN;
+use ring::aead::Nonce;
+use ring::aead::NonceSequence;
+use ring::aead::OpeningKey;
+use ring::aead::SealingKey;
+use ring::aead::UnboundKey;
+use ring::aead::AES_256_GCM;
+use ring::aead::Aad;
+use ring::aead::BoundKey;
 
-use ring::{
-    aead::AES_256_GCM,
-    rand::{SecureRandom, SystemRandom},
-};
+use ring::error::Unspecified;
+use ring::rand::{SecureRandom, SystemRandom};
+
+pub struct CounterNonceSequence(pub u32);
+
+impl NonceSequence for CounterNonceSequence {
+    // called once for each seal operation
+    fn advance(&mut self) -> Result<Nonce, Unspecified> {
+        let mut nonce_bytes = vec![0; NONCE_LEN];
+
+        let bytes = self.0.to_be_bytes();
+        nonce_bytes[8..].copy_from_slice(&bytes);
+
+        self.0 += 1; // advance the counter
+        Nonce::try_assume_unique_for_key(&nonce_bytes)
+    }
+}
 
 pub fn create_random_aes_key() -> Vec<u8> {
     let rand = SystemRandom::new();
@@ -25,4 +47,39 @@ pub fn write_key_to_env_file(key: &Vec<u8>) {
 
     // Write some content to the file.
     writeln!(file, "AES_KEY={}", hex::encode(key)).unwrap();
+}
+
+pub fn encrypt(
+    data: Vec<u8>,
+    key_bytes: Vec<u8>,
+    nonce_sequence: CounterNonceSequence,
+) -> Result<Vec<u8>, Unspecified> {
+    // Create a new AEAD key without a designated role or nonce sequence
+    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
+
+    // Create a new AEAD key for encrypting and signing ("sealing"), bound to a nonce sequence
+    // The SealingKey can be used multiple times, each time a new nonce will be used
+    let mut sealing_key = SealingKey::new(unbound_key, nonce_sequence);
+
+    // Create a mutable copy of the data that will be encrypted in place
+    let mut in_out = data.clone();
+
+    // Encrypt the data with AEAD using the AES_256_GCM algorithm
+    let tag = sealing_key.seal_in_place_separate_tag(Aad::empty(), &mut in_out)?;
+
+    // Decrypt the data by passing in the associated data and the cypher text with the authentication tag appended
+    let cypher_text_with_tag = [&in_out, tag.as_ref()].concat();
+
+    Ok(cypher_text_with_tag)
+}
+
+pub fn decrypt(
+    mut cypher_text_with_tag: Vec<u8>,
+    key_bytes: Vec<u8>,
+    nonce_sequence: CounterNonceSequence,
+) -> Result<Vec<u8>, Unspecified> {
+    let unbound_key = UnboundKey::new(&AES_256_GCM, &key_bytes)?;
+    let mut opening_key = OpeningKey::new(unbound_key, nonce_sequence);
+    let decrypted_data = opening_key.open_in_place(Aad::empty(), &mut cypher_text_with_tag)?;
+    Ok(decrypted_data.to_vec())
 }
