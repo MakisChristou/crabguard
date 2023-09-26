8d5ebfc57423a842c5e88a78e635e845c067d0cb
Makis Christou <michalis.christou@protonmail.com>
2023-09-26 19:23:17 +0300

    Refactor: Upload Command

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 
@@ -20,6 +21,60 @@ mod utils;
 #[derive(Serialize, Deserialize, Debug)]
 struct Data {
     filenames: HashMap<String, Vec<u8>>,
+}
+
+fn encrypt_and_upload_data_chunk(
+    data: &Vec<u8>,
+    file_name: &str,
+    key_bytes: Vec<u8>,
+    storage: &impl Storage,
+) -> Result<(), Unspecified> {
+    let nonce_sequence = CounterNonceSequence::new_random();
+    let starting_value = nonce_sequence.0.to_vec();
+
+    let cypher_text_with_tag = utils::encrypt(data.clone(), key_bytes.clone(), nonce_sequence)?;
+
+    // Prepend the nonce on the ciphertext
+    let mut data_to_store = starting_value;
+    data_to_store.extend_from_slice(&cypher_text_with_tag);
+
+    let plaintext_filename = file_name;
+    let hashed_filename = format!("{}", hex::encode(Sha256::digest(&plaintext_filename)));
+
+    storage.upload(&hashed_filename, &data_to_store).unwrap();
+
+    Ok(())
+}
+
+fn encrypt_and_upload_name_chunk(
+    file_name: &OsStr,
+    filenames: &mut HashMap<String, Vec<u8>>,
+    key_bytes: Vec<u8>,
+    storage: &impl Storage,
+) -> Result<(), Unspecified> {
+    let plaintext_filename = file_name.to_str().unwrap();
+    let hashed_filename = format!("{}", hex::encode(Sha256::digest(&plaintext_filename)));
+
+    // Update the HashMap
+    let starting_value = &Sha256::digest(&plaintext_filename)[..NONCE_LEN];
+
+    let nonce_array: [u8; NONCE_LEN] = starting_value.try_into().unwrap();
+    let nonce_sequence = CounterNonceSequence::new(nonce_array);
+    let encrypted_name = utils::encrypt(
+        plaintext_filename.try_into().unwrap(),
+        key_bytes.clone(),
+        nonce_sequence,
+    )?;
+    // Update the HashMap in RAM
+    let mut name_blob: Vec<u8> = Vec::new();
+    name_blob.extend_from_slice(&starting_value);
+    name_blob.extend_from_slice(&encrypted_name);
+    filenames.insert(hashed_filename, name_blob);
+
+    let encoded: Vec<u8> = bincode::serialize(&filenames).unwrap();
+    storage.upload(HASHMAP_NAME, &encoded).unwrap();
+
+    Ok(())
 }
 
 fn main() -> Result<(), Unspecified> {
@@ -40,45 +95,17 @@ fn main() -> Result<(), Unspecified> {
         Some(Commands::Upload { file_path }) => {
             let data = fs::read(file_path).unwrap();
 
-            let nonce_sequence = CounterNonceSequence::new_random();
-            let starting_value = nonce_sequence.0.to_vec();
-
-            let cypher_text_with_tag =
-                utils::encrypt(data.clone(), key_bytes.clone(), nonce_sequence)?;
+            encrypt_and_upload_data_chunk(&data, &file_path, key_bytes.clone(), &local_storage)?;
 
             let path = Path::new(file_path);
+
             if let Some(file_name) = path.file_name() {
-                // Prepend the nonce on the ciphertext
-                let mut data_to_store = starting_value;
-                data_to_store.extend_from_slice(&cypher_text_with_tag);
-
-                let plaintext_filename = file_name.to_str().unwrap();
-                let hashed_filename =
-                    format!("{}", hex::encode(Sha256::digest(&plaintext_filename)));
-
-                local_storage
-                    .upload(&hashed_filename, &data_to_store)
-                    .unwrap();
-
-                // Update the HashMap
-                let starting_value = &Sha256::digest(&plaintext_filename)[..NONCE_LEN];
-                let nonce_array: [u8; NONCE_LEN] = starting_value.try_into().unwrap();
-                let nonce_sequence = CounterNonceSequence::new(nonce_array);
-                let encrypted_name = utils::encrypt(
-                    plaintext_filename.try_into().unwrap(),
-                    key_bytes,
-                    nonce_sequence,
-                )
-                .unwrap();
-
-                // Update the HashMap in RAM
-                let mut name_blob: Vec<u8> = Vec::new();
-                name_blob.extend_from_slice(&starting_value);
-                name_blob.extend_from_slice(&encrypted_name);
-                filenames.insert(hashed_filename, name_blob);
-
-                let encoded: Vec<u8> = bincode::serialize(&filenames).unwrap();
-                local_storage.upload(HASHMAP_NAME, &encoded).unwrap();
+                encrypt_and_upload_name_chunk(
+                    file_name,
+                    &mut filenames,
+                    key_bytes.clone(),
+                    &local_storage,
+                )?;
             } else {
                 panic!("Path given does not contain filename");
             }
